44b4ef84c8b3244d78a36d83bb573e97feac9dc0
Unknown <rggb@icomp.ufam.edu.br>
2017-08-22 16:40:15 -0400

    [UPD] Writting

--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,17 @@
 int main(int argc, char* argv[])
 {
     IOHandler handler(fopen("./files/pattern.csv", "r"));
-    Block_t block;
-    FILE* blockFile = fopen("./test/penises.block", "wb+");
-
+    FILE* blockFile = fopen("./test/pattern.block", "wb+");
+  
+    Article_t currentArticle;
+    
     while (handler.hasNext()) {
-        Article_t article;
-        handler >> article;
-        block.tryPutArticle(article);
-        fwrite(&block, sizeof(Block_t), 1, blockFile);
-        fclose(blockFile);
-        blockFile = fopen("./test/penises.block", "rb");
-        Block_t anotherBlock;
-        fread(&anotherBlock, sizeof(Block_t), 1, blockFile);
-        // std::cout << article.toString() << "\n\n"
-        //           << std::endl;
-        std::cout << anotherBlock.getArticle(0)->toString() << "\n\n"
-                  << std::endl;
-        handler.parseNext();
+        Block_t currentBlock;
+        
+        do{
+            handler >> currentArticle;
+        }while(currentBlock.tryPutArticle(currentArticle));
+
     }
+
 }
