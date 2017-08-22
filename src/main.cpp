bc6f195a508292f2dc8d76c4dc1b8440d5a0b84b
Unknown <rggb@icomp.ufam.edu.br>
2017-08-22 12:08:59 -0400

    [TST] Block Reading Test

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,27 @@
 #include "block.hpp"
 #include "iohandler.hpp"
 
+#include <cstdlib>
 #include <iostream>
+
 int main(int argc, char* argv[])
 {
     IOHandler handler(fopen("./files/pattern.csv", "r"));
     Block_t block;
+    FILE* blockFile = fopen("./test/penises.block", "wb+");
 
     while (handler.hasNext()) {
         Article_t article;
         handler >> article;
         block.tryPutArticle(article);
+        fwrite(&block, sizeof(Block_t), 1, blockFile);
+        fclose(blockFile);
+        blockFile = fopen("./test/penises.block", "rb");
+        Block_t anotherBlock;
+        fread(&anotherBlock, sizeof(Block_t), 1, blockFile);
         // std::cout << article.toString() << "\n\n"
         //           << std::endl;
-        std::cout << block.getArticle(0)->toString() << "\n\n"
+        std::cout << anotherBlock.getArticle(0)->toString() << "\n\n"
                   << std::endl;
         handler.parseNext();
     }
